82d41237fc55de81f2b80b65957284a1c07fd942
Gerald <gerald050011@hotmail.com>
2026-02-20 19:15:45 -0600

    FirstUpdate#1

---
+++ WebApplicationAPP/WebApplicationAPP/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationAPP.Models;
+
+namespace WebApplicationAPP.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Persona> Persona { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Inventario> Inventario { get; set; }
+        //public DbSet<WebApplicationAPP.Models.Producto> Productos { get; set; }
+    }
+}
